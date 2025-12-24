c437fa516bc85924dfb6291139abdcaa0073e900
Kanchan Nath <kanchannath.webdev@gmail.com>
2025-12-24 20:38:05 +0530

    Fix comment controller user validation and update video model default isPublished value

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,7 +3,7 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {User} from "../models/user.model.js"
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
@@ -13,6 +13,25 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    // user - validate
+    // user cooment from body
+
+    const {content} = req.body
+
+    if(!content){
+        throw new ApiError(400, "Field is Required")
+    }
+
+    const user = await User.findById(req.user?._id)
+
+    if(!user){
+        throw new ApiError(400, "Invalid User")
+    }
+
+    console.log(content)
+    
+
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
