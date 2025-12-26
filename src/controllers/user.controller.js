42f8c848423a3ae0dec12d68c17fed0a513c50bb
Kanchan Nath <kanchannath.webdev@gmail.com>
2025-12-26 11:58:36 +0530

    Fix avatar file path assignment in registerUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -58,7 +58,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
     let avatarLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        avatarLocalPath = req.files.coverImage[0].path
+        avatarLocalPath = req.files.avatar[0].path
     }
 
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
