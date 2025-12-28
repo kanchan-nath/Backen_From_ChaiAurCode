9fcfd04376b872923e278844635afc355fc71954
Kanchan Nath <kanchannath.webdev@gmail.com>
2025-12-28 21:05:19 +0530

    Enhance video upload functionality by updating file handling and adding deleteCloudinary utility; enforce duration requirement in video model

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,10 +1,10 @@
-import mongoose, {isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId, mongo} from "mongoose"
 import {Video} from "../models/video.model.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { uploadOnCloudinary, deleteCloudinary } from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -26,25 +26,31 @@ const publishAVideo = asyncHandler(async (req, res) => {
     }
 
     let videoFileLocalPath;
-    if(req.file && Array.isArray(req.file.videoFile) && req.file.videoFile.length > 0){
-        videoFileLocalPath = req.file.videoFile[0].path
+    if(req.files && Array.isArray(req.files.videoFile) && req.files.videoFile.length > 0){
+        videoFileLocalPath = req.files.videoFile[0].path
     }
 
     if (!videoFileLocalPath){
-        throw new ApiError(400, "Video File is required X")
+        throw new ApiError(400, "Video File is required ")
     }
 
     let thumbnailLocalPath;
-    if(req.file && Array.isArray(req.file.thumbnail) && req.file.thumbnail.length > 0){
-        thumbnailLocalPath = req.file.thumbnail[0].path
+    if(req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0){
+        thumbnailLocalPath = req.files.thumbnail[0].path
     }
 
     if(!thumbnailLocalPath){
-        throw new ApiError(400, " Thumbanil is required Y")
+        throw new ApiError(400, " Thumbanil is required ")
     }
 
     const video = await uploadOnCloudinary(videoFileLocalPath)
     const thumbnail  = await uploadOnCloudinary(thumbnailLocalPath)
+
+    // console.log(video.public_id) 
+    //duration: 10.01
+    const duration = video.duration
+    const videoPublicId = video.public_id
+    const thumbnailPublicId = thumbnail.public_id
 
     if(!video){
         throw new ApiError(400, "Video file is required")
@@ -54,16 +60,20 @@ const publishAVideo = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Thumbnail file is required")
     }
 
-
-    await Video.create({
+    const videoDetails = await Video.create({
         title,
         description, 
-        video: video.url,
-        thumbnail: thumbnail.url
-
-        
+        videoFile: video.url,
+        thumbnail: thumbnail.url,      
+        duration,
+        views: 56,
+        isPublished: true,
     })
 
+    return res
+    .status(200)
+    .json(new ApiResponse(200, { videoDetails, videoPublicId, thumbnailPublicId }, "Video Uploaded Successfully")) 
+    // 694f99b0ba060cfbe56f45f6
     
 })
 
@@ -71,22 +81,17 @@ const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
 
-    const {title, description} = req.body
+    const isValid = mongoose.Types.ObjectId.isValid(videoId)
 
-    const video = await Video.findByIdAndUpdate(
-        req.user?._id,
-        {
-            $set:{
-                title,
-                description,
-            }
-        }
-    )
+    if(!isValid){
+        throw new ApiError(400, "VideoId is not Valid")
+    }
+
+    const video  = await Video.findById(videoId)
 
     return res
     .status(200)
-    .json(new ApiResponse(200, video, "Changed Successfully"))
-
+    .json(new ApiResponse(400, video, "Video Found Success"))
 
 })
 
@@ -94,13 +99,62 @@ const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
 
+    const isValid = mongoose.Types.ObjectId.isValid(videoId)
 
+    if(!isValid){
+        throw new ApiResponse(400, "Video Not Found")
+    }
+
+    const {title, description} = req.body
+    let changeThumbnailLocalPath
+
+    if(req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length>0){
+        changeThumbnailLocalPath = req.files.thumbnail[0].path
+    }
+    const changedThumbnail = await uploadOnCloudinary(changeThumbnailLocalPath)
+    console.log(changedThumbnail)
+
+    const updatedVideoDetails = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set:{
+                title,
+                description,
+                thumbnail: changedThumbnail.url,
+            }
+        },
+        {new:true}
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedVideoDetails, "Video Details Changed Succesfully"))
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const { videoPublicId, thumbnailPublicId } = req.body
     //TODO: delete video
+
+    //Delete from Database
+    
+    const videoDelete = await Video.findByIdAndDelete(videoId)    
+
+    if(!videoDelete){
+        throw new ApiError(400, "Video Not Found")
+    }
+
+    //Delete from Cloudinary - Currently not working
+
+    const videoPublicIdDeleted = await deleteCloudinary(videoPublicId)
+    const thumbnailPublicIdDeleted = await deleteCloudinary(thumbnailPublicId)
+
+    // console.log(cloudinaryAssetsDeleted)
+
+    return res
+    .status(200)
+    .json(200, videoDelete, "Video Deleted")
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,6 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
-
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
@@ -12,20 +11,34 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
+        const response = await cloudinary.uploader.upload(
+            localFilePath, 
+            {
+            resource_type: "auto",
+            folder: "backend_assets"
+
         })
         // file has been uploaded successfull
         // console.log("file is uploaded on cloudinary ", response.url);
+        console.log(response)
         fs.unlinkSync(localFilePath)
         return response;
-
+        
     } catch (error) {
-        // fs.unlinkSync("Fs UnlinkSync Error",localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync("Fs UnlinkSync Error",localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
 
-
-
-export {uploadOnCloudinary}
+const deleteCloudinary = async (cloudinaryPublicId)=>{
+    try {
+        await cloudinary.uploader
+        .destroy(cloudinaryPublicId)
+    } catch (error) {
+        console.log("Clodinary Delete Assets Error", error)
+    }
+}
+export {
+    uploadOnCloudinary,
+    deleteCloudinary
+}
