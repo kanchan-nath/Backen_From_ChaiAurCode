801181c72056cb8c964b7bd1f8adc62c5d5d02a4
Kanchan Nath <kanchannath.webdev@gmail.com>
2026-01-02 13:52:17 +0530

    Video Streaming Leraning from Chai-aur code

---
+++ video-streaming-main/frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+import videojs from "video.js";
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/courses/08b0425c-9e76-476d-9712-8f6a61756485/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
